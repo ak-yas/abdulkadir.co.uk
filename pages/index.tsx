11d9dd01fe8bbe1868ef483931d6493ee80e82c0
ak-yas <lolesp295@gmail.com>
2022-07-11 19:10:10 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div style={{ display: 'grid', placeContent: 'center', height: '100vh' }}>
+      <h1>With styled components</h1>
+    </div>
+  )
+}
