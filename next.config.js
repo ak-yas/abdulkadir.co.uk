b83ad320f82685e3f52007a54d4f38a5d4103321
ak-yas <lolesp295@gmail.com>
2022-07-14 20:57:39 +0100

    added image config, remove title from _doc

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['cdn.sanity.io', 'cdn.pixabay.com'],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 }
 
